fa4341436956b032836b06c5bb51497316e9e35a
censek <censek.chen@belstar.com.cn>
2020-05-22 16:38:48 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+// 解决移动端300ms延迟问题
+import fastClick from 'fastclick';
 import App from './App.vue';
 import router from './router';
+import './assets/styles/reset.css';
+// 移动端1像素边框问题
+import './assets/styles/border.css';
 
 Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 new Vue({
   router,
